8c016442ffb0eba82b40c27a84a96c2b5c874a4c
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-04-09 08:33:51 -0700

    Finished work

---
+++ table.js
@@ -0,0 +1,95 @@
+
+
+function Thestores (minCookies, maxCookies, averageCookies, id, name) {
+  this.minCookies = minCookies;
+  this.maxCookies = maxCookies;
+  this.averageCookies = averageCookies;
+  this.id = id;
+  this.name = name;
+  this.cookieCal = function () {
+    return Math.floor(Math.random() * (this.maxCookies - this.minCookies + 1)) + this.minCookies;
+  }
+}
+
+var pioneerThestores = new Thestores (17, 88, 5.2, "pioneersq", "Pioneer Square");
+var airportThestores = new Thestores (6, 24, 1.2, "portlandAir", "Portland Airport");
+var squareThestores = new Thestores (11, 38, 1.9, "washingtonsq", "Washington Square");
+var sellwoodThestores = new Thestores (20, 48, 3.3, "sellwood", "Sellwood");
+var pearlThestores = new Thestores (3, 24, 2.6, "pdistrict", "Pearl District");
+
+var stores = new Array ();
+stores.push(pioneerThestores);
+stores.push(airportThestores);
+stores.push(squareThestores);
+stores.push(sellwoodThestores);
+stores.push(pearlThestores);
+
+function writeStoreHours (hours) {
+  switch (hours) {
+    case 0: return "10am - ";
+    case 1: return "11am - ";
+    case 2: return "12pm - ";
+    case 3: return "1pm - ";
+    case 4: return "2pm - ";
+    case 5: return "3pm - ";
+    case 6: return "4pm - ";
+    case 7: return "5pm - ";
+  }
+}
+
+function writeStoreData (store) {
+  var container = document.getElementById("storetable");
+  var rowdata = "<tr><td>" + store.name + "</td>";
+  var totalCookies = 0;
+  for(t = 0; t < 8; t++) {
+    var cookies = store.cookieCal();
+    rowdata += "<td>" + writeStoreHours(t)+ cookies + "</td>";
+    totalCookies += cookies;
+  }
+  container.innerHTML += rowdata + "<td>Total: " + totalCookies + "</td></tr>" ;
+}
+
+function buildStorePage () {
+  var container = document.getElementById("storetable");
+  container.innerHTML = "";
+  for (index = 0; index < stores.length; index++) {
+    writeStoreData(stores[index])
+  }
+}
+buildStorePage();
+
+
+function addStores() {
+  var form = document.getElementById("SubmitFreshStores");
+  var name = form.storeName.value;
+  var min = parseInt(form.numberCustomerMin.value);
+  var max = parseInt(form.numberCustomerMax.value);
+  var average = parseFloat(form.averageNumberCustomer.value);
+  var newStore = new Thestores(min, max, average, "", name);
+
+  stores.push(newStore);
+  buildStorePage();
+}
+
+function validateInput () {
+  var validElement = document.getElementsByClassName("pickChoice");
+  var pickedQuestion = 0;
+
+  for (j = 0; j < validElement.length; j++){
+    if (validElement[j] === ""){
+      validElement[j].style.borderColor="rgba(35, 0, 189, 1)";
+    }
+    else {
+      validElement[j].style.borderColor="white";
+      pickedQuestion++;
+    }
+  }
+  if (pickedQuestion === validElement.length) {
+    var button = document.getElementById("submitButton");
+    addStores();
+  }
+}
+
+window.addEventListener("load",buildStorePage);
+window.addEventListener("load",writeStoreData);
+element.addEventListener('click', validateInput, false);
